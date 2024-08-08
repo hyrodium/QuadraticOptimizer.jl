072c1990a79a64f3352f65257999478161cdfddd
Yuto Horikawa <hyrodium@gmail.com>
2024-08-08 20:37:32 +0900

    replace `X` with `X'`

--- src/QFM_n-dim.jl
+++ src/QFM_n-dim.jl
@@ -5,11 +5,11 @@ function _recursion_qfm!(f, ps::Vector{<:SVector{D, <:Real}}, F::Vector, X::Matr
     F[i] = f(p)
     j = 1
     for i1 in 1:D, i2 in i1:D
-        X[i,j] = p[i1]*p[i2]
+        X[j,i] = p[i1]*p[i2]
         j = j + 1
     end
-    X[i,M+1:M+D] .= p
-    Y = pinv(X'*X)*(X'*F)
+    X[M+1:M+D, i] .= p
+    Y = pinv(X*X')*(X*F)
     a = Y[SOneTo(M)]
     b = SVector{D}(Y[M+1:M+D])
     c = Y[end]
@@ -21,9 +21,9 @@ function optimize_qfm!(f, ps::Vector{<:SVector{D, <:Real}}, n::Integer) where D
     L = length(ps)
     M = D*(D+1)÷2
     N = D+M+1
-    L ≤ N && error("The length of initial values should be larger than $(N).")
+    L < N && error("The length of initial values should be larger than $(N).")
     F = zeros(L)
-    X = ones(L, N)
+    X = ones(N, L)
     for i in 1:N-1
         p = ps[i]
         F[i] = f(p)
@@ -32,7 +32,7 @@ function optimize_qfm!(f, ps::Vector{<:SVector{D, <:Real}}, n::Integer) where D
             X[i,j] = p[i1]*p[i2]
             j = j + 1
         end
-        X[i,M+1:M+D] .= p
+        X[M+1:M+D, i] .= p
     end
     for _ in 1:n
         p = _recursion_qfm!(f, ps, F, X, L)
--- src/QIM_n-dim.jl
+++ src/QIM_n-dim.jl
@@ -5,11 +5,11 @@ function _recursion_qim!(f, ps::Vector{<:SVector{D, <:Real}}, F::MVector{N}, X::
     F[i] = f(p)
     j = 1
     for i1 in 1:D, i2 in i1:D
-        X[i,j] = p[i1]*p[i2]
+        X[j,i] = p[i1]*p[i2]
         j = j + 1
     end
-    X[i,M+1:M+D] .= p
-    Y = pinv(X) * F
+    X[M+1:M+D, i] .= p
+    Y = pinv(X') * F
     a = Y[SOneTo(M)]
     b = SVector{D}(Y[M+1:M+D])
     c = Y[end]
@@ -24,15 +24,15 @@ function optimize_qim!(f, ps::Vector{<:SVector{D, <:Real}}, n::Integer) where D
     L ≠ N && error("The length of initial values should be equal to $(N).")
     F = @MVector zeros(N)
     X = @MMatrix ones(N,N)
-    for i in 1:N-1
+    for i in 1:N
         p = ps[i]
         F[i] = f(p)
         j = 1
         for i1 in 1:D, i2 in i1:D
-            X[i,j] = p[i1]*p[i2]
+            X[j,i] = p[i1]*p[i2]
             j = j + 1
         end
-        X[i,M+1:M+D] .= p
+        X[M+1:M+D, i] .= p
     end
     for _ in 1:n
         p = _recursion_qim!(f, ps, F, X)
